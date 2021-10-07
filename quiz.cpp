c44f5fceb02a423ebf6b7619093d26566ade71d3
Hoa Thiên Vũ <hoathienvu8x@gmail.com>
2021-10-07 19:19:20 +0700

    Simple question -> answer handle

--- quiz.cpp
+++ quiz.cpp
@@ -1,7 +1,15 @@
+#include <algorithm>
 #include <iostream>
 #include <cstring>
 #include <exception>
 #include <sqlite.h>
+
+std::string lower(std::string str) {
+    std::transform(str.begin(), str.end(), str.begin(), [](unsigned char c) {
+        return std::tolower(c);
+    });
+    return str;
+}
 
 int main(int argc, char **argv) {
     std::cout << "Mini Quiz" << std::endl;
@@ -17,6 +25,16 @@ int main(int argc, char **argv) {
         } catch (std::exception& ex) {
             std::cout << ex.what() << std::endl;
         }
+    }
+    std::string question;
+    while(1) {
+        std::cout << "Question: ";
+        std::cin >> question;
+        question = lower(question);
+        if (question == "quit" || question == "edit") {
+            break;
+        }
+        std::cout << "Answer: " << question << std::endl;
     }
     return 0;
 }
