004b6d885662c6746524ddbdd6b92baffc89d936
Hoa Thiên Vũ <hoathienvu8x@gmail.com>
2021-10-08 18:12:18 +0700

    Update code

--- quiz.cpp
+++ quiz.cpp
@@ -13,28 +13,55 @@ std::string lower(std::string str) {
 
 int main(int argc, char **argv) {
     std::cout << "Mini Quiz" << std::endl;
-    if (argc > 1 && strcmp(argv[1], "initdb") == 0) {
-        try {
-            database db("quiz.db");
-            command cmd(db, "CREATE TABLE quiz (id INTEGER NOT NULL,content VARCHAR NOT NULL,tags VARCHAR(200) NOT NULL,PRIMARY KEY (id));");
-            if(cmd.execute() == SQLITE_OK) {
-                std::cout << "OK" << std::endl;
-            } else {
-                std::cout << "FAIL" << std::endl;
-            }
-        } catch (std::exception& ex) {
-            std::cout << ex.what() << std::endl;
-        }
+    if (argc <= 1) {
+        std::cout << "Usage: quiz [initdb] [import <filepath>] [keyword]" << std::endl;
+        std::cout << "    initdb: Initialize database" << std::endl;
+        std::cout << "    import: Import to database with filepath" << std::endl;
+        std::cout << "    keyword: Search in database" << std::endl;
+        exit(0);
     }
-    std::string question;
-    while(1) {
-        std::cout << "Question: ";
-        std::cin >> question;
-        question = lower(question);
-        if (question == "quit" || question == "edit") {
-            break;
+    if (argc > 1) {
+        if (strcmp(argv[1], "initdb") == 0) {
+            try {
+                database db("quiz.db");
+                command cmd(db, "CREATE TABLE quiz (id INTEGER NOT NULL,content VARCHAR NOT NULL,tags VARCHAR(200) NOT NULL,PRIMARY KEY (id));");
+                if(cmd.execute() == SQLITE_OK) {
+                    std::cout << "OK" << std::endl;
+                } else {
+                    std::cout << "FAIL" << std::endl;
+                }
+            } catch (std::exception& ex) {
+                std::cout << ex.what() << std::endl;
+            }
+            exit(0);
+        } else if (strcmp(argv[1],"import") == 0) {
+            if (argc < 3) {
+                std::cout << "Usage: quiz import <filepath>" << std::endl;
+                exit(0);
+            }
+            std::string filepath = std::string(argv[2]);
+            if (filepath.size() == 0) {
+                std::cout << "Usage: quiz import <filepath>" << std::endl;
+                exit(0);
+            }
+            std::cout << filepath << std::endl;
+            exit(0);
         }
-        std::cout << "Answer: " << question << std::endl;
+        std::string keyword = std::string(argv[1]);
+        if (keyword.size() == 0) {
+            std::cout << "Nothing to search" << std::endl;
+            exit(0);
+        }
+        std::string tag = "";
+        if (argc > 2) {
+            tag = std::string(argv[2]);
+        }
+        if (tag.size() > 0) {
+            std::cout << "[" << tag << "] " << keyword << std::endl;
+        } else {
+            std::cout << keyword << std::endl;
+        }
+        exit(0);
     }
     return 0;
 }
